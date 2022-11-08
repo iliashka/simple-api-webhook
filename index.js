9d7247f31ff5cc6c82a77972927bab25e28ce73f
ilyaZubko <dev1_documint@brightdot.tech>
2022-11-08 13:44:30 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false }))
+
+app.use(bodyParser.json({ extended: true }));
+
+app.post('/api/webhook', ((req, res) => {
+  try {
+    console.log(req.body)
+    res.status(200).json("good")
+  } catch (error) {
+    console.log(error)
+  }
+}));
+
+app.listen(PORT, () => {
+  console.log('Сервер загружен на порту: ', PORT)
+})
